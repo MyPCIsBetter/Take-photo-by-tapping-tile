68869e6dfa45f0de980e8780bacfb1f6769839e1
Piotrek <piotrekk1@outlook.com>
2016-05-03 20:38:43 +0200

    First commit

--- TapTileToTakePhoto/MainPage.xaml.cs
+++ TapTileToTakePhoto/MainPage.xaml.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.StartScreen;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,55 @@ namespace TapTileToTakePhoto
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void addAppButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Uri messemgerTileImage = new Uri("ms-appx:///Assets/LockScreenLogo.scale-200.png");
+
+                // During creation of secondary tile, an application may set additional arguments on the tile that will be passed in during activation.
+                // These arguments should be meaningful to the application. In this sample, we'll pass in the date and time the secondary tile was pinned.
+                //string tileActivationArguments = "SecondaryTile.Messenger" + " WasPinnedAt=" + DateTime.Now.ToLocalTime().ToString();
+                string tileActivationArguments = "takephoto";
+
+                SecondaryTile takePhotoTile = new SecondaryTile("SecondaryTile.TakePhoto", "TakePhoto", tileActivationArguments, messemgerTileImage, TileSize.Square150x150);
+
+                bool isPinned = await takePhotoTile.RequestCreateForSelectionAsync(GetElementRect((FrameworkElement)sender), Windows.UI.Popups.Placement.Below);
+
+                Debug.WriteLine("Pinned: " + isPinned);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception: " + ex.Data);
+            }
+        }
+
+        // Gets the rectangle of the element
+        public static Rect GetElementRect(FrameworkElement element)
+        {
+            GeneralTransform buttonTransform = element.TransformToVisual(null);
+            Point point = buttonTransform.TransformPoint(new Point());
+            return new Rect(point, new Size(element.ActualWidth, element.ActualHeight));
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //e.Parameter - tileActivationArguments kafelka
+            if (!String.IsNullOrEmpty(e.Parameter.ToString()))
+            {
+                try
+                {
+                    //Nie można wywołać Navigate jeśli aktualny się nie zakończył
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                             () => Frame.Navigate(typeof(PhotoThanApp), e.Parameter));
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+            }
         }
     }
 }
---
+++ TapTileToTakePhoto/PhotoThanApp.xaml.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Sensors;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.FileProperties;
+using Windows.Storage.Streams;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TapTileToTakePhoto
+{
+    /// <summary>
+    /// Page that will take photo
+    /// </summary>
+    public sealed partial class PhotoThanApp : Page
+    {
+        private MediaCapture _mediaCapture;
+        private bool _isInitialized;
+
+        private bool _externalCamera;
+        private bool _mirroringPreview;
+        private bool _isPreviewing;
+        private static readonly Guid RotationKey = new Guid("C380465D-2271-428C-9B83-ECEA3B4A85C1");
+
+        private SimpleOrientation _deviceOrientation = SimpleOrientation.NotRotated;
+
+        public PhotoThanApp()
+        {
+            this.InitializeComponent();
+
+            Application.Current.Resuming += Application_Resuming;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await InitializeCameraAsync();
+
+            try
+            {
+                //e.Parameter - name of tile
+                //if (true || (e.Parameter != null && !String.IsNullOrEmpty(e.Parameter.ToString())))
+                //{
+                    await TakePhotoAsync();
+                //}
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+
+        }
+
+        private async void Application_Resuming(object sender, object o)
+        {
+            // Handle global application events only if this page is active
+            if (Frame.CurrentSourcePageType == typeof(PhotoThanApp))
+            {
+                await InitializeCameraAsync();
+            }
+        }
+
+        private async Task InitializeCameraAsync()
+        {
+
+            if (_mediaCapture == null)
+            {
+
+                // Get available devices for capturing pictures
+                var allVideoDevices = await DeviceInformation.FindAllAsync(DeviceClass.VideoCapture);
+
+                // Get the desired camera by panel
+                DeviceInformation cameraDevice =
+                    allVideoDevices.FirstOrDefault(x => x.EnclosureLocation != null &&
+                    x.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front);
+
+                // If there is no camera on the specified panel, get any camera
+                cameraDevice = cameraDevice ?? allVideoDevices.FirstOrDefault();
+
+                if (cameraDevice == null)
+                {
+                    //ShowMessageToUser("No camera device found.");
+                    Debug.WriteLine("No camera device found.");
+                    return;
+                }
+
+                // Create MediaCapture and its settings
+                _mediaCapture = new MediaCapture();
+
+                // Register for a notification when video recording has reached the maximum time and when something goes wrong
+                //_mediaCapture.RecordLimitationExceeded += MediaCapture_RecordLimitationExceeded;
+
+                var mediaInitSettings = new MediaCaptureInitializationSettings { VideoDeviceId = cameraDevice.Id };
+
+                // Initialize MediaCapture
+                try
+                {
+                    await _mediaCapture.InitializeAsync(mediaInitSettings);
+                    _isInitialized = true;
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    //ShowMessageToUser("The app was denied access to the camera");
+                    Debug.WriteLine("The app was denied access to the camera");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Exception when initializing MediaCapture with {0}: {1}", cameraDevice.Id, ex.ToString());
+                }
+
+                // If initialization succeeded, start the preview
+                if (_isInitialized)
+                {
+                    // Figure out where the camera is located
+                    if (cameraDevice.EnclosureLocation == null || cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Unknown)
+                    {
+                        // No information on the location of the camera, assume it's an external camera, not integrated on the device
+                        _externalCamera = true;
+                    }
+                    else
+                    {
+                        // Camera is fixed on the device
+                        _externalCamera = false;
+
+                        // Only mirror the preview if the camera is on the front panel
+                        //_mirroringPreview = (cameraDevice.EnclosureLocation.Panel == Windows.Devices.Enumeration.Panel.Front);
+                    }
+
+                    //await StartPreviewAsync();
+
+                    //UpdateCaptureControls();
+                }
+            }
+        }
+
+        private async Task TakePhotoAsync()
+        {
+            var stream = new InMemoryRandomAccessStream();
+
+            try
+            {
+                Debug.WriteLine("Taking photo...");
+                await _mediaCapture.CapturePhotoToStreamAsync(ImageEncodingProperties.CreateJpeg(), stream);
+                Debug.WriteLine("Photo taken!");
+
+                var photoOrientation = ConvertOrientationToPhotoOrientation(GetCameraOrientation());
+                await ReencodeAndSavePhotoAsync(stream, "photo.jpg", photoOrientation);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception when taking a photo: {0}", ex.ToString());
+            }
+
+        }
+
+        private SimpleOrientation GetCameraOrientation()
+        {
+            if (_externalCamera)
+            {
+                // Cameras that are not attached to the device do not rotate along with it, so apply no rotation
+                return SimpleOrientation.NotRotated;
+            }
+
+            // If the preview is being mirrored for a front-facing camera, then the rotation should be inverted
+            if (_mirroringPreview)
+            {
+                // This only affects the 90 and 270 degree cases, because rotating 0 and 180 degrees is the same clockwise and counter-clockwise
+                switch (_deviceOrientation)
+                {
+                    case SimpleOrientation.Rotated90DegreesCounterclockwise:
+                        return SimpleOrientation.Rotated270DegreesCounterclockwise;
+                    case SimpleOrientation.Rotated270DegreesCounterclockwise:
+                        return SimpleOrientation.Rotated90DegreesCounterclockwise;
+                }
+            }
+
+            return _deviceOrientation;
+        }
+
+        private static PhotoOrientation ConvertOrientationToPhotoOrientation(SimpleOrientation orientation)
+        {
+            switch (orientation)
+            {
+                case SimpleOrientation.Rotated90DegreesCounterclockwise:
+                    return PhotoOrientation.Rotate90;
+                case SimpleOrientation.Rotated180DegreesCounterclockwise:
+                    return PhotoOrientation.Rotate180;
+                case SimpleOrientation.Rotated270DegreesCounterclockwise:
+                    return PhotoOrientation.Rotate270;
+                case SimpleOrientation.NotRotated:
+                default:
+                    return PhotoOrientation.Normal;
+            }
+        }
+
+        private static async Task ReencodeAndSavePhotoAsync(IRandomAccessStream stream, string filename, PhotoOrientation photoOrientation)
+        {
+            using (var inputStream = stream)
+            {
+                var decoder = await BitmapDecoder.CreateAsync(inputStream);
+
+                var file = await KnownFolders.PicturesLibrary.CreateFileAsync(filename, CreationCollisionOption.GenerateUniqueName);
+
+                using (var outputStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                {
+                    var encoder = await BitmapEncoder.CreateForTranscodingAsync(outputStream, decoder);
+
+                    var properties = new BitmapPropertySet { { "System.Photo.Orientation", new BitmapTypedValue(photoOrientation, PropertyType.UInt16) } };
+
+                    await encoder.BitmapProperties.SetPropertiesAsync(properties);
+                    await encoder.FlushAsync();
+                }
+            }
+        }
+    }
+}
